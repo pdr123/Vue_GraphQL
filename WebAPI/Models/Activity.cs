0874ab110d6a8268158e2cb5c1fd0a177def3ee9
pdr123 <pinnintidurgarao123@gmail.com>
2023-07-08 16:06:05 +0530

    Scripts Creation

--- WebAPI/Models/Activity.cs
+++ WebAPI/Models/Activity.cs
@@ -5,9 +5,10 @@ namespace WebAPI.Models
     public class Activity
     {
         [Key]
-        public int Id { get; set; }
+        public int ActivityId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int InchargeId { get; set; }
+        public bool BolActive { get; set; }
     }
 }
--- WebAPI/Models/Result.cs
+++ WebAPI/Models/Result.cs
@@ -1,11 +1,16 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
 {
     public class Result
     {
-        public int Id { get; set; }
+        [Key]
+        public int ResultId { get; set; }
         public int NoOfAttempts { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
+        public int StatusId { get; set; }
         public ResultStatus Status { get; set; }
         public int ActivityId { get; set; }
         public int DurationHours { get; set; }
