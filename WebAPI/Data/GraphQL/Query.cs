0a4e5712796d402439e6657589ae83248c470dce
pdr123 <pinnintidurgarao123@gmail.com>
2023-07-08 15:16:30 +0530

    Query Added

--- WebAPI/Data/GraphQL/Query.cs
+++ WebAPI/Data/GraphQL/Query.cs
@@ -1,6 +1,18 @@
-﻿namespace WebAPI.Data.GraphQL
+﻿using WebAPI.Data.Repositories;
+using WebAPI.Models;
+
+namespace WebAPI.Data.GraphQL
 {
     public class Query
     {
+        public List<User> GetAllUsers([Service] UserRepository userRepository) 
+        {
+            return userRepository.getAllUsers();
+        }
+
+        public List<Result> GetResultsByUser([Service] ResultRepository resultRepository, int userId) 
+        {
+            return resultRepository.GetResultsByUser(userId);
+        }
     }
 }
